50f9f61c37825facdf6483b01812d6e6767b7710
Glapson <glapsable@gmail.com>
2019-02-14 18:57:07 +0100

    setup webpack babel webpack-dev-server

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+const template = <p>This is JSX from app.js</p>;
+
+ReactDOM.render(template, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+    mode: "development",
+    entry: './src/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'public')
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: "babel-loader"
+        }]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    }
+};
